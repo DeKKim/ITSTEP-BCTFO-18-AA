1888651964c7ffedb6ead5c90eefd06d97387167
DeKKim <125554291+DeKKim@users.noreply.github.com>
2024-02-27 11:15:10 +0400

    Add files via upload

---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int a = random.Next(1, 6);
+
+        if (a == 1)
+        {
+            Console.WriteLine("Hint: Yvitelia");
+            string word = "banani";
+            char[] guessedWord = new char[word.Length];
+            for (int i = 0; i < guessedWord.Length; i++)
+            {
+                guessedWord[i] = '_';
+            }
+
+            bool wordGuessed = false;
+            int cda = 0;
+            while (!wordGuessed)
+            {
+                Console.WriteLine(string.Join(" ", guessedWord));
+
+                Console.Write("Sheiyvanet Aso: ");
+                char input = char.ToLower(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+
+                bool letterFound = false;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == input)
+                    {
+                        guessedWord[i] = input;
+                        letterFound = true;
+                    }
+                }
+
+                if (!guessedWord.Contains('_'))
+                {
+                    wordGuessed = true;
+                }
+
+                cda++;
+
+                if (!letterFound)
+                {
+                    Console.WriteLine("Arasworia! Kidev Scadet");
+                }
+            }
+
+            Console.WriteLine("Gilocavt! Tqven Warmatebit Gamoicanit Sityva '{0}'  {1} Cdashi.", word, cda);
+        }
+        else if (a == 2)
+        {
+            Console.WriteLine("Hint: Am Sasiamovno Nivtierebas Didi Istoria aqvs Da is Mesoamerikuli Kulturebidan Momdinareobs");
+            
+            string word = "shokoladi";
+            char[] guessedWord = new char[word.Length];
+            for (int i = 0; i < guessedWord.Length; i++)
+            {
+                guessedWord[i] = '_';
+            }
+
+            bool wordGuessed = false;
+            int cda = 0;
+            while (!wordGuessed)
+            {
+                Console.WriteLine(string.Join(" ", guessedWord));
+
+                Console.Write("Sheiyvanet Aso: ");
+                char input = char.ToLower(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+
+                bool letterFound = false;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == input)
+                    {
+                        guessedWord[i] = input;
+                        letterFound = true;
+                    }
+                }
+
+                if (!guessedWord.Contains('_'))
+                {
+                    wordGuessed = true;
+                }
+
+                cda++;
+
+                if (!letterFound)
+                {
+                    Console.WriteLine("Arasworia! Kidev Scadet");
+                }
+            }
+
+            Console.WriteLine("Gilocavt! Tqven Warmatebit Gamoicanit Sityva '{0}'  {1} Cdashi.", word, cda);
+
+        }
+        else if (a == 3)
+        {
+            Console.WriteLine("Hint: Xshirad Am Nivts Samzareuloshi Vxvdebit Da Aramarto Mand");
+           
+            string word = "dana";
+            char[] guessedWord = new char[word.Length];
+            for (int i = 0; i < guessedWord.Length; i++)
+            {
+                guessedWord[i] = '_';
+            }
+
+            bool wordGuessed = false;
+            int cda = 0;
+            while (!wordGuessed)
+            {
+                Console.WriteLine(string.Join(" ", guessedWord));
+
+                Console.Write("Sheiyvanet Aso: ");
+                char input = char.ToLower(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+
+                bool letterFound = false;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == input)
+                    {
+                        guessedWord[i] = input;
+                        letterFound = true;
+                    }
+                }
+
+                if (!guessedWord.Contains('_'))
+                {
+                    wordGuessed = true;
+                }
+
+                cda++;
+
+                if (!letterFound)
+                {
+                    Console.WriteLine("Arasworia! Kidev Scadet");
+                }
+            }
+
+            Console.WriteLine("Gilocavt! Tqven Warmatebit Gamoicanit Sityva '{0}'  {1} Cdashi.", word, cda);
+        }
+        else if (a == 4)
+        {
+            Console.WriteLine("Hint: Chven Mas Yoveldgiurad Viyenebt");
+            
+            string word = "kompiuteri";
+            char[] guessedWord = new char[word.Length];
+            for (int i = 0; i < guessedWord.Length; i++)
+            {
+                guessedWord[i] = '_';
+            }
+
+            bool wordGuessed = false;
+            int cda = 0;
+            while (!wordGuessed)
+            {
+                Console.WriteLine(string.Join(" ", guessedWord));
+
+                Console.Write("Sheiyvanet Aso: ");
+                char input = char.ToLower(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+
+                bool letterFound = false;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == input)
+                    {
+                        guessedWord[i] = input;
+                        letterFound = true;
+                    }
+                }
+
+                if (!guessedWord.Contains('_'))
+                {
+                    wordGuessed = true;
+                }
+
+                cda++;
+
+                if (!letterFound)
+                {
+                    Console.WriteLine("Arasworia! Kidev Scadet");
+                }
+            }
+
+            Console.WriteLine("Gilocavt! Tqven Warmatebit Gamoicanit Sityva '{0}'  {1} Cdashi.", word, cda);
+        }
+        else if (a == 5)
+        {
+            Console.WriteLine("Hint: Am Metals Xshirad Iyeneben Mwedlebi");
+            string word = "rkina";
+            char[] guessedWord = new char[word.Length];
+            for (int i = 0; i < guessedWord.Length; i++)
+            {
+                guessedWord[i] = '_';
+            }
+
+            bool wordGuessed = false;
+            int cda = 0;
+            while (!wordGuessed)
+            {
+                Console.WriteLine( string.Join(" ", guessedWord));
+
+                Console.Write("Sheiyvanet Aso: ");
+                char input = char.ToLower(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+
+                bool letterFound = false;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == input)
+                    {
+                        guessedWord[i] = input;
+                        letterFound = true;
+                    }
+                }
+
+                if (!guessedWord.Contains('_'))
+                {
+                    wordGuessed = true;
+                }
+
+                cda++;
+
+                if (!letterFound)
+                {
+                    Console.WriteLine("Arasworia! Kidev Scadet");
+                }
+            }
+
+            Console.WriteLine("Gilocavt! Tqven Warmatebit Gamoicanit Sityva '{0}'  {1} Cdashi.", word, cda);
+        }
+    }
+}
---
+++ რიცხვის გამოცნობა/რიცხვის გამოცნობა/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int randomnumber = random.Next(1, 10);
+        int randomnumber1 = random.Next(1, 101);
+        int Cda = 0;
+        Console.WriteLine("Airchie Tamashis Versiis Nomeri");
+        Console.WriteLine("1.Short Version(1 dan 10 mde 3 cda)");
+        Console.WriteLine("2.Long Version(1 dan 100 mde Ulimito cda)");
+        int cda1 = 3;
+        if (int.TryParse(Console.ReadLine(), out int x))
+        {
+            if (x == 1)
+            {
+                Console.WriteLine("Random Ricxvi Archeulia");
+                Console.WriteLine("Darchenili Cda: "+cda1);
+                for (int i = 0; i < 3; i++)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int a))
+                    {
+                        if (a == randomnumber)
+                        {
+                            Console.WriteLine("Gilocavt Tqven Sworad Gamoicanit Ricxvi");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pasuxi Arasworia Kidev Scadet");
+                            cda1--;
+                            Console.WriteLine("Darchenili Cda: " + cda1);
+                            if (randomnumber > a)
+                            {
+                                Console.WriteLine("Chafiqrebuli Ricxvi Magalia Tqvens Ricxvze");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Chafiqrebuli Ricxvi Dabalia Tqvens Ricxvze");
+                            }
+                            if (cda1 == 0)
+                                
+                           
+                            {
+                                Console.WriteLine("Swori Pasuxi Iyo: "+randomnumber);
+                                    }
+                        }
+                    }
+                 
+                }
+            }
+            else if (x == 2)
+            {
+                Console.WriteLine("Random Ricxvi Archeulia"+" "+randomnumber1);
+               for(int i = 0;i<1;)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int b))
+                    {
+                        if (b == randomnumber1)
+                        {
+                            
+                            Cda++;
+                            Console.WriteLine("Gilocavt Tqven Sworad Gamoicanit Ricxvi");
+                            Console.WriteLine("Swori Pasuxi Gamoicanit: "+Cda+" Cdashi");
+                            break;
+                        }
+                        else
+                        {
+                            Cda++;
+                            Console.WriteLine("Pasuxi Arasworia Kidev Scadet");
+                        }
+                    }
+                   
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+      
+    }
+}
